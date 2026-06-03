1227f94ae8d62924feb7f2ddd14d3b54a8612000
wh1texdg <kostaskrebkov1@gmail.com>
2026-06-03 14:13:46 +0300

    add start, help handlers and db connection

---
+++ bot.py
@@ -0,0 +1,68 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+import asyncpg
+import asyncio
+from dotenv import load_dotenv
+import os
+from database import create_tables
+
+load_dotenv()
+pool = None
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start_command(message: Message):
+    await message.answer("""
+    👋 Привет! Я Reminder Bot - помогу не забыть о важных дедлайнах.
+
+🔥 Что я умею:
+- Добавлять дедлайны 
+- Отправлять уведомление с напоминанием
+- Показывать список твоих задач
+
+Напиши /help, чтобы увидеть все команды.
+""")
+
+@dp.message(Command("help"))
+async def help_command(message: Message):
+    await message.answer("""
+📋 Список команд:
+
+/start - Запуск бота
+
+/add - Добавить новый дедлайн
+Бот спросит название задачи и дату дедлайна.
+
+/list - Показать все твои задачи
+Выводит список активных дедлайнов.
+
+/done - Отметить задачу выполненной
+Задача удаляется из списка активных.
+
+/delete - Удалить задачу
+Удаляет задачу из списка.
+
+/cancel - Отменить текущее действие
+Используй, если хочешь прервать добавление задачи.
+    """)
+
+
+async def main():
+    global pool
+    pool = await asyncpg.create_pool(
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT"))
+    )
+    async with pool.acquire() as conn:
+        await create_tables(conn)
+    
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main()) 
